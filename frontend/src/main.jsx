36eabc8f4a307d23dfeb817b2b8d7cf2233db08f
hacksh4w <mrinalsnair@gmail.com>
2023-10-19 07:14:09 +0530

    fix : resolve version issues with vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import header from './pages/header'
-import Headingpage from './pages/header'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <Headingpage/>
+   <App />
   </React.StrictMode>,
 )
