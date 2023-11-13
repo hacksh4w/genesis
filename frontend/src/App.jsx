03d5e8f5f16c66e303a9a191d1c3a597396faf83
Mary Ann Jose <maryannjose129@gmail.com>
2023-11-13 19:19:09 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 //import { useState, useContext } from 'react'
 import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import Signup from './pages/SignUpPage/Signup'
 import LandingPage from './pages/LandingPage/LandingPage'
 import { Routes, Route } from "react-router-dom"
 import Footer from './components/Footer/Footer'
+import DonorApplication from './pages/donorApplication'
 
 const App = () => {
   return (
     <div className='App'>
-      <Routes>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/signin" element={<Signup/>}></Route>
-        <Route path="/home" element={<LandingPage/>}></Route>
-      {/*} <Route path="/Genesis/adminlogin" element={<Signup/>}></Route>
+      <ChakraProvider>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signup />}></Route>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/donorapplication" element={<DonorApplication/>}></Route>
+          {/*} <Route path="/Genesis/adminlogin" element={<Signup/>}></Route>
         <Route path="/Genesis/transactions" element={<Signup/>}></Route>
         <Route path="/Genesis/signup" element={<Signup/>}></Route> */}
-      </Routes>
-      <Footer />
+        </Routes>
+        <Footer />
+      </ChakraProvider>
     </div>
   )
 }
