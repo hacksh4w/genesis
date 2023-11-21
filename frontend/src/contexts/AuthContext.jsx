436f348ba20adac26f04a43a45f4369f33b35f06
hacksh4w <mrinalsnair@gmail.com>
2023-11-21 09:10:28 +0530

    stash

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { supabase } from '../config/config';
 
 const AuthContext = createContext();
