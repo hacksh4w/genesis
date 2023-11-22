850faa1866929be17c69ec589f82aacfbf048a84
Mrinalini Nair <mrinalsnair@gmail.com>
2023-11-22 12:30:45 +0530

    feat : connect cart & verify transaction

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -1,57 +1,39 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../config/config'; 
-import { useAuth } from "../contexts/AuthContext";
+import { supabase } from '../config/config';
+import { useAuth } from '../contexts/AuthContext';
 import { useToast } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
+import { Button, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 
 const RequestsTable = () => {
   const [requests, setRequests] = useState([]);
-  const AuthContext=useAuth()
-  const userId = AuthContext.userID
-  const toast = useToast({})
+  const AuthContext = useAuth();
+  const userId = AuthContext.userID;
+  const toast = useToast({});
+
   useEffect(() => {
-
-    /*try {
-        const { data:Data, error: errorData } = await supabase
-            .from('adopter_basic_info')
-            .select()
-            .eq('user_id', userid)
-
-        setbasicInfo(Data[0])
-
-        if (errorData) throw errorData
-    } catch (error) {
-        toast({
-            title: "failed to fetch adopter profile details",
-            status: "error",
-            isClosable: true,
-            position: "top"
-        }) */
     const fetchRequests = async () => {
       try {
-        const { data:Data, error:errorData } = await supabase
-                .from('requests')
-                .select('*')
-                //.eq('userId', userId);
-            setRequests(Data[0])
-        if (errorData) throw errorData
+        const { data: Data, error: errorData } = await supabase
+          .from('Cart')
+          .select('*');
+        if (errorData) throw errorData;
         else {
           setRequests(Data || []);
-        } }catch(error) {
-            toast({
-                  title: "failed to fetch adopter profile details",
-                  status: "error",
-                  isClosable: true,
-                  position: "top"
-              })
+        }
+      } catch (error) {
+        toast({
+          title: 'Failed to fetch requests',
+          status: 'error',
+          isClosable: true,
+          position: 'top',
+        });
       }
     };
     fetchRequests();
-  }, []);
+  }, [toast]);
 
   const handleVerifyRequest = async (requestId) => {
     try {
-      // Update the 'verified' field in the 'requests' table to true
       const { data, error } = await supabase
         .from('requests')
         .update({ verified: true })
@@ -61,7 +43,6 @@ const RequestsTable = () => {
         console.error('Error updating request:', error);
       } else {
         console.log('Request verified successfully:', data);
-        // Optional: Update local state to reflect the change
         setRequests((prevRequests) =>
           prevRequests.map((req) =>
             req.id === requestId ? { ...req, verified: true } : req
@@ -75,40 +56,41 @@ const RequestsTable = () => {
 
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>User ID</th>
-            <th>Donor ID</th>
-            <th>Sample ID</th>
-            <th>Verified</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table variant="striped" colorScheme="teal" size="sm">
+        <Thead>
+          <Tr>
+            <Th>User ID</Th>
+            <Th>Donor ID</Th>
+            <Th>Sample ID</Th>
+            <Th>Verified</Th>
+            <Th>Actions</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
           {requests.map((request) => (
-            <tr key={request.id}>
-              <td>{request.id}</td>
-              <td>{request.radopter_id}</td>
-              <td>{request.rdonor_id}</td>
-              <td>{request.rclinicadmin_id}</td>
-              <td>{request.rsample_id}</td>
-              <td >{request.verified ? 'Yes' : 'No'}</td>
-              <td>
-              {!request.verified ? (
+            <Tr key={request.id}>
+              <Td>{request.id}</Td>
+              <Td>{request.price}</Td>
+              <Td>{request.donor_id}</Td>
+              <Td>{request.adopter_id}</Td>
+              {/*<Td>{request.rsample_id}</Td> */}
+              <Td>{request.verified ? 'Yes' : 'No'}</Td>
+              <Td>
+                {!request.verified ? (
                   <Button onClick={() => handleVerifyRequest(request.id)}>
                     Verify Request
                   </Button>
                 ) : (
-                    <Button disabled>Verified</Button>
+                  <Button disabled>Verified</Button>
                 )}
-              </td>
-            </tr>
+              </Td>
+            </Tr>
           ))}
-        </tbody>
-      </table>
+        </Tbody>
+      </Table>
     </div>
   );
 };
 
 export default RequestsTable;
+
